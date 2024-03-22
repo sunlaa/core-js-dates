efe7fd50dc38316f8db2dbc022261393570a4d63
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-22 19:19:26 +0100

    feat: solve problem #6

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -110,8 +110,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const data1 = new Date(dateStart);
+  const data2 = new Date(dateEnd);
+
+  return 1 + new Date(data2 - data1) / 1000 / 60 / 60 / 24;
 }
 
 /**
