c32c217cfa59e54d46817c1cff725ce9622e6e09
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-22 19:07:01 +0100

    feat: solve problem #5

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -93,8 +93,10 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month);
+  date.setDate(date.getDate() - 1);
+  return date.getDate();
 }
 
 /**
