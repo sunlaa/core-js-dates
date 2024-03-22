08524461eaa5974d907e7bd6c5eac13f5cef8c1a
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-22 18:56:44 +0100

    feat: solve problem #4

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -76,8 +76,10 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const shift = date.getDay() === 5 ? 7 : (5 - date.getDay() + 7) % 7;
+  date.setDate(date.getDate() + shift);
+  return date;
 }
 
 /**
