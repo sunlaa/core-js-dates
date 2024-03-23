e265d73649e8505b40006789568d094c33fc43b5
Uladzislava Santalava <santalova.lada25@gmail.com>
2024-03-23 15:26:42 -0100

    feat: solve last problem

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -358,8 +358,12 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const february = new Date(date.getUTCFullYear(), 2, 1);
+  february.setUTCDate(february.getUTCDate() - 1);
+
+  if (february.getUTCDate() === 29) return true;
+  return false;
 }
 
 module.exports = {
